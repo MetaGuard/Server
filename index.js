60de70a910c75b7e1245af9fa9de7dc77fdc9052
Vivek Nair <vivek@nair.me>
2022-07-06 13:43:19 -0700

    Game Server

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const ws = require('ws');
+
+const wss = new ws.WebSocketServer({ port: 8080 });
+
+var perSecond = 0;
+
+wss.on('connection', function connection(ws) {
+  console.log('client connected!');
+
+  ws.on('message', function message(data) {
+    // console.log('received: %s', data);
+    perSecond++;
+  });
+});
+
+setInterval(() => {
+  console.log('requests per second: ' + perSecond);
+  perSecond = 0;
+}, 1000);
